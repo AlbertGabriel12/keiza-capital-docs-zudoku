eabe6f625454d79a064fc9d147ae568a3390dab7
Albert Keiyza <albertgabriel051@gmail.com>
2025-06-16 21:09:31 -0300

    feat: update paymentPix route

--- openApiGenerator/docs/routes/auth/paymentPix/index.js
+++ openApiGenerator/docs/routes/auth/paymentPix/index.js
@@ -5,12 +5,14 @@ const sandboxOnlyPay = require("./sandboxOnlyPay");
 const getEarnedFeesBalance = require("./earnedSelfFees/getEarnedFeesBalance");
 const withdrawEarnedFees = require("./earnedSelfFees/withdrawEarnedFees");
 const getByPaymentId = require("./getByPaymentId");
+const update = require("./update");
 
 module.exports = {
     "/auth/payment/pix": {
         ...getAll,
         ...create,
     },
+    ...update,
     ...getById,
     ...getByPaymentId,
     ...sandboxOnlyPay,
