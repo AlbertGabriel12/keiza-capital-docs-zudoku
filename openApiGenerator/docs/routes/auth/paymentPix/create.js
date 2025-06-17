8e6e2999693563e9b7d28007e6d3332819129e54
Igor Luís <igoraclima2@gmail.com>
2025-06-17 17:40:57 -0300

    mensagem do que mudou

--- openApiGenerator/docs/routes/auth/paymentPix/create.js
+++ openApiGenerator/docs/routes/auth/paymentPix/create.js
@@ -28,6 +28,10 @@ module.exports = {
                             "dueDate": {
                                 "type": "string",
                                 "example": "2025-04-12"
+                            },
+                            "callbackSuccessUrl": {
+                                "type": "string",
+                                "example": "https://seudominio.com/sucesso"
                             }
                         },
                         "required": [
@@ -40,8 +44,30 @@ module.exports = {
         },
         "responses": {
             "201": {
-                "description": "",
-                "headers": {}
+                "description": "Pagamento criado",
+                "content": {
+                    "application/json": {
+                        "example": {
+                            "id": "1f70b77b-08bf-486b-a6ac-a18e1781f146",
+                            "paymentId": "PkEgXNmbu33FzB5Nxah",
+                            "externalId": "string",
+                            "checkoutPaymentLink": "https://sandbox-keiza-capital.vercel.app/checkoutPix/1f70b77b-08bf-486b-a6ac-a18e1781f146",
+                            "callbackSuccessUrl": "https://seudominio.com/sucesso",
+                            "amount": 10,
+                            "netAmount": 9,
+                            "description": "string",
+                            "dueDate": null,
+                            "fees": {
+                                "selfEarnedFee": null,
+                                "infraFee": 1
+                            },
+                            "payload": "00020126580014br.gov.bcb.pix01366e49949c-424b-4ba7-8734-d4f1fd2dc272520400005303986540510.005802BR5917Brla Digital Ltda6009Sao Paulo6229052500004LPkEgXNmbu33FzB5Nxah6304EF57",
+                            "status": "PENDING",
+                            "createdAt": "2025-06-16T23:23:46.136Z",
+                            "updatedAt": "2025-06-16T23:23:46.136Z"
+                        }
+                    }
+                }
             },
             "400": {
                 "description": "",
@@ -58,3 +84,4 @@ module.exports = {
         ]
     }
 }
+;
